255f05fd06fbddc125e5e3592d52f0cf6d3b2a15
ManishThulung <manish.thulung58@gmail.com>
2024-04-15 20:04:55 +0545

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+import Transfer from "@/components/transfer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Transfer />
+    </>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Image from "next/image";
+import React, { FC } from "react";
+
+const Navbar: FC = () => {
+  return (
+    <div className="bg-slate-800 flex justify-around items-center py-4">
+      <Image
+        alt="solana-logo"
+        src="/solanaLogo.png"
+        height={30}
+        width={200}
+        className="text-white"
+      />
+      <h3 className="text-white">Wallet-Adapter Example</h3>
+      <WalletMultiButton />
+    </div>
+  );
+};
+
+export default Navbar;
